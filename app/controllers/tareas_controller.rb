e54202fc4fdaca7c97c4fe1c6d3f3f690892d1f5
MarcoJavier3 <mark720c@gmail.com>
2018-10-02 16:53:54 -0500

    first proyect rubyonrails

---
+++ app/controllers/tareas_controller.rb
@@ -0,0 +1,13 @@
+class TareasController < ApplicationController
+  def index
+    @tareas = Tarea.all 
+    # select * from tareas
+  end
+
+  def new 
+    @tarea = Tarea.new
+  end
+  def create
+  end
+  
+end
