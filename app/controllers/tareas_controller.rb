6e7e22874d8c65157196894bf88c9f1e57581367
MarcoJavier3 <mark720c@gmail.com>
2018-10-05 10:43:58 -0500

    version con metodos update

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -4,22 +4,50 @@ class TareasController < ApplicationController
     # select * from tareas
   end
 
+  def show
+    @tarea = Tarea.find(params[:id])
+   # redirect_to controller: "tareas" , action: "show"
+       #select * from tareas where id=
+  end
+
   def new 
     @tarea = Tarea.new
   end
+
   def create
     @tarea = Tarea.new(titulo: params[:tarea][:titulo], descripcion: params[:tarea][:descripcion])
     @tarea.save
   if  @tarea.save
     #insert into tareas
+    redirect_to controller: 'tareas', action: 'show' , id: @tarea.id 
   else
     render :new
    end
     
   end
+
   # insert into tareas(titulo, descripcion) values (formulario)
-  def show
-    @tarea = Tarea.find(params [:id])
-    #select * from tareas where id=
+ 
+
+  def destroy
+    @tarea = Tarea.find(params[:id])
+    @tarea.destroy
+    redirect_to controller: "tareas" , action: "index"
   end
+
+  def edit
+    @tarea = Tarea.find(params[:id])
+    redirect_to controller: "tareas" , action: "edit"
+  end
+
+  def update
+    @tarea = Tarea.find(params[:id])
+    if @tarea.update(titulo: params[:tarea][:titulo],
+      descripcion: params[:tarea][:descripcion])
+      redirect_to @tarea
+    else
+      render :new
+    end
+
+
 end
